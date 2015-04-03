a4c5848028da7fce77d5ab01ae044124f1cc3806
bryan.absher <bryan.absher@lab49.com>
2015-04-03 15:40:02 -0400

    added json creator

--- grok-core-api/src/main/java/grok/core/request/SearchRequest.java
+++ grok-core-api/src/main/java/grok/core/request/SearchRequest.java
@@ -1,5 +1,6 @@
 package grok.core.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.geojson.LngLatAlt;
@@ -7,6 +8,7 @@ import org.geojson.LngLatAlt;
 @AutoValue
 public abstract class SearchRequest {
 
+  @JsonCreator
   public static SearchRequest of(@JsonProperty("search") String search,
                                  @JsonProperty("location") LngLatAlt location) {
     return new AutoValue_SearchRequest.Builder_AutoValue_SearchRequest()
