7ce489f1fda91ff0c7cb83fa07b1827ea256ff0d
bryan.absher <bryan.absher@lab49.com>
2015-05-16 21:42:39 -0400

    updated users object with new fields

--- grok-core-api/src/main/java/grok/core/User.java
+++ grok-core-api/src/main/java/grok/core/User.java
@@ -1,18 +1,32 @@
 package grok.core;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+
+import javax.annotation.Nullable;
 
 @AutoValue
 public abstract class User {
 
-  public static User of(long id, String name) {
-    return new AutoValue_User(id, name);
+  @JsonCreator
+  public static User of(@JsonProperty("_id") String id,
+                        @JsonProperty("name") String name,
+                        @JsonProperty("location") String location,
+                        @JsonProperty("email") String email) {
+    return new AutoValue_User(id, name, location, email);
   }
 
   @JsonProperty
-  public abstract long id();
+  @Nullable
+  public abstract String id();
 
   @JsonProperty
   public abstract String name();
+
+  @JsonProperty
+  public abstract String location();
+
+  @JsonProperty
+  public abstract String email();
 }
