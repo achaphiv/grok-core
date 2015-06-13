d3987dd7c0f7da8c05c4665bfc31266e106c3541
Alex Chaphiv <achaphiv@gmail.com>
2015-06-13 17:49:40 -0400

    added missing jackson annotations

--- grok-core-api/src/main/java/grok/core/CragUpdate.java
+++ grok-core-api/src/main/java/grok/core/CragUpdate.java
@@ -3,21 +3,34 @@ package grok.core;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import org.mongojack.Id;
 
+import javax.annotation.Nullable;
 import java.util.Date;
 
 @AutoValue
 public abstract class CragUpdate {
 
   @JsonCreator
-  public static CragUpdate of(String clientId, Date date, Crag crag) {
-    return new AutoValue_CragUpdate(clientId, date, crag);
+  public static CragUpdate of(
+      @Id @JsonProperty("id") String id,
+      @JsonProperty("client") String clientId,
+      @JsonProperty("date") Date date,
+      @JsonProperty("crag") Crag crag) {
+    return new AutoValue_CragUpdate(id, clientId, date, crag);
   }
+
+  @Id
+  @Nullable
+  @JsonProperty
+  public abstract String id();
 
   @JsonProperty
   public abstract String clientId();
+
   @JsonProperty
   public abstract Date date();
+
   @JsonProperty
   public abstract Crag crag();
 }
--- grok-core-api/src/main/java/grok/core/RouteUpdate.java
+++ grok-core-api/src/main/java/grok/core/RouteUpdate.java
@@ -3,16 +3,26 @@ package grok.core;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import org.mongojack.Id;
 
+import javax.annotation.Nullable;
 import java.util.Date;
 
 @AutoValue
 public abstract class RouteUpdate {
-
   @JsonCreator
-  public static RouteUpdate of(String clientId, Date date, Route route) {
-    return new AutoValue_RouteUpdate(clientId, date, route);
+  public static RouteUpdate of(
+      @Id @JsonProperty("id") String id,
+      @JsonProperty("client") String clientId,
+      @JsonProperty("date") Date date,
+      @JsonProperty("route") Route route) {
+    return new AutoValue_RouteUpdate(id, clientId, date, route);
   }
+
+  @Id
+  @Nullable
+  @JsonProperty
+  public abstract String id();
 
   @JsonProperty
   public abstract String clientId();
