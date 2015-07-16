e092ec5331ad505ef6257d81bfbc1b72e737bd26
Alex Chaphiv <achaphiv@gmail.com>
2015-07-16 17:08:31 -0400

    Remove unused import

--- grok-core-api/src/main/java/grok/core/Id.java
+++ grok-core-api/src/main/java/grok/core/Id.java
@@ -2,12 +2,10 @@ package grok.core;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.auto.value.AutoValue;
 
 @AutoValue
 public abstract class Id {
-
   @JsonCreator
   public static Id of(@JsonProperty("id") String id) {
     return new AutoValue_Id(id);
