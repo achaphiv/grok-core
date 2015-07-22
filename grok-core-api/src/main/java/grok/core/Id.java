22ccd14e6b2e779dac773abe2f0250428dc66c1e
Alex Chaphiv <achaphiv@gmail.com>
2015-07-22 18:39:04 -0400

    Id: use as @JsonValue type instead of a complex object

--- grok-core-api/src/main/java/grok/core/Id.java
+++ grok-core-api/src/main/java/grok/core/Id.java
@@ -1,18 +1,23 @@
 package grok.core;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.auto.value.AutoValue;
 
 @AutoValue
 public abstract class Id {
   @JsonCreator
-  public static Id of(@JsonProperty("id") String id) {
+  public static Id of(String id) {
     return new AutoValue_Id(id);
   }
 
   Id() {}
 
-  @JsonProperty
-  public abstract String id();
+  @Deprecated
+  public String id() {
+    return value();
+  }
+
+  @JsonValue
+  public abstract String value();
 }
