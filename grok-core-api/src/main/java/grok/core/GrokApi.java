5909e0f626e2847234f79cc6425bc1ec72593db3
Alex Chaphiv <achaphiv@gmail.com>
2015-12-02 04:53:17 -0500

    Remove usage of feign

--- grok-core-api/src/main/java/grok/core/GrokApi.java
+++ grok-core-api/src/main/java/grok/core/GrokApi.java
@@ -1,113 +1,14 @@
 package grok.core;
 
-import java.util.List;
+public final class GrokApi {
+  public static final String CRAGS_ENDPOINT = "/crags";
+  public static final String CRAGS_ID_ENDPOINT = "/crags/{id}";
+  public static final String CRAGS_ID_ROUTES_ENDPOINT = "/crags/{id}/routes";
+  public static final String ROUTES_ENDPOINT = "/routes";
+  public static final String ROUTES_ID_ENDPOINT = "/routes/{id}";
+  public static final String ROUTES_WITH_CRAGS_ENDPOINT = "/routes/withCrag";
+  public static final String ROUTES_WITH_CRAGS_ID_ENDPOINT = "/routes/{id}/withCrag";
+  public static final int DEFAULT_PAGE_SIZE = 10;
 
-import feign.Headers;
-import feign.Param;
-import feign.RequestLine;
-
-public interface GrokApi {
-  String CRAGS_ENDPOINT = "/crag";
-  String ROUTES_ENDPOINT = "/route";
-  int DEFAULT_PAGE_SIZE = 10;
-
-  @RequestLine("GET " + CRAGS_ENDPOINT)
-  List<Crag> crags();
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/{id}")
-  Crag crag(@Param("id") Id id);
-
-  @RequestLine("POST " + CRAGS_ENDPOINT)
-  @Headers("Content-Type: application/json")
-  void insertCrag(Crag c);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/{id}/routes")
-  List<Route> getRoutesForCrag(@Param("id") Id id);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/{id}/routes/?start={start}&size={size}")
-  List<Route> getRoutesForCrag(@Param("id") Id id,
-                               @Param("start") int start,
-                               @Param("size") int size);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/{id}")
-  Route route(@Param("id") String id);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/{id}/withCrag")
-  RouteWithCrag routeWithCrag(@Param("id") Id id);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/withCrag/?query={query}&lat={lat}&lng={lng}&start={start}&size={size}")
-  List<RouteWithCrag> routesWithCrag(
-      @Param("query") String query,
-      @Param("lat") String lat,
-      @Param("lng") String lng,
-      @Param("start") int start,
-      @Param("size") int pageSize);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/withCrag/?query={query}&crag={crag}&start={start}&size={size}")
-  List<RouteWithCrag> routesWithCrag(
-      @Param("query") String query,
-      @Param("crag") Id id,
-      @Param("start") int start,
-      @Param("size") int pageSize);
-
-  @RequestLine("POST " + ROUTES_ENDPOINT)
-  @Headers("Content-Type: application/json")
-  void insertRoute(Route r);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/?query={query}&lat={lat}&lng={lng}&start={start}&size={size}")
-  List<Route> searchRoutes(@Param("query") String query,
-                           @Param("lat") Double lat,
-                           @Param("lng") Double lng,
-                           @Param("start") int start,
-                           @Param("size") int size);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/?query={query}&start={start}&size={size}")
-  List<Route> searchRoutes(@Param("query") String query,
-                           @Param("start") int start,
-                           @Param("size") int size);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/?query={query}")
-  List<Route> searchRoutes(@Param("query") String query);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/?query={query}&crag={crag}&lat={lat}&lng={lng}&start={start}&size={size}")
-  List<Route> searchRoutes(@Param("query") String query,
-                           @Param("crag") Id crag,
-                           @Param("lat") Double lat,
-                           @Param("lng") Double lng,
-                           @Param("start") int start,
-                           @Param("size") int size);
-
-  @RequestLine("GET " + ROUTES_ENDPOINT + "/?query={query}&crag={crag}")
-  List<Route> searchRoutes(@Param("query") String query,
-                           @Param("crag") Id crag);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/?query={query}&lat={lat}&lng={lng}&start={start}&size={size}")
-  List<Crag> searchCrags(@Param("query") String query,
-                         @Param("lat") Double latitude,
-                         @Param("lng") Double longitude,
-                         @Param("start") int start,
-                         @Param("size") int size);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/?query={query}&lat={lat}&lng={lng}")
-  List<Crag> searchCrags(@Param("query") String query,
-                         @Param("lat") Double latitude,
-                         @Param("lng") Double longitude);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/?lat={lat}&lng={lng}")
-  List<Crag> searchCrags(@Param("lat") Double latitude,
-                         @Param("lng") Double longitude);
-
-  @RequestLine("GET " + CRAGS_ENDPOINT + "/?lat={lat}&lng={lng}&start={start}&size={size}")
-  List<Crag> searchCrags(@Param("lat") Double latitude,
-                         @Param("lng") Double longitude,
-                         @Param("start") int start,
-                         @Param("size") int size);
-
-  @RequestLine("PUT " + CRAGS_ENDPOINT + "/")
-  @Headers("Content-Type: application/json")
-  Id update(Crag c);
-
-  @RequestLine("PUT " + ROUTES_ENDPOINT + "/")
-  @Headers("Content-Type: application/json")
-  Id update(Route r);
+  private GrokApi() {}
 }
